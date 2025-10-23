d65f8a27e0248ef05f06c7c10bc67a12d4b9b45f
Vivian Nguyen <yuemannguyen@gmail.com>
2025-10-23 09:53:33 -0700

    Update to full octave

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,4 +1,4 @@
-// 7 button keyboard using a uniform resistor ladder (6 x 10kΩ).
+// Full octave keyboard using a uniform resistor ladder (7 x 10kΩ).
 
 // Daisy Bell
 // 4 2 1 0 0 1 2 3 5 4
@@ -15,7 +15,7 @@
 // 4 4 3 3 2 2 1 4 4 3 3 2 2 1
 // 0 0 4 4 5 5 4 3 3 2 2 1 1 0
 
-const int notes[] = {262, 294, 330, 349, 392, 440, 494};
+const int notes[] = {262, 294, 330, 349, 392, 440, 494, 523};
 
 void setup() {
   Serial.begin(9600);
@@ -25,20 +25,22 @@ void loop() {
   int keyVal = analogRead(A0);
   Serial.println(keyVal);
 
-  if(keyVal == 1023){      // C: 262Hz
+  if(keyVal >= 1000){      // C: 262Hz
     tone(8, notes[0]);
-  }else if(keyVal >= 500){ // D: 294Hz
+  }else if(keyVal >= 495){ // D: 294Hz
     tone(8, notes[1]);
-  }else if(keyVal >= 330){ // E: 330Hz
+  }else if(keyVal >= 325){ // E: 330Hz
     tone(8, notes[2]);
-  }else if(keyVal >= 250){ // F: 349Hz
+  }else if(keyVal >= 245){ // F: 349Hz
     tone(8, notes[3]);
-  }else if(keyVal >= 200){ // G: 392Hz
+  }else if(keyVal >= 195){ // G: 392Hz
     tone(8, notes[4]);
-  }else if(keyVal >= 160){ // A: 440Hz
+  }else if(keyVal >= 155){ // A: 440Hz
     tone(8, notes[5]);
-  }else if(keyVal >= 140){ // B: 494Hz
+  }else if(keyVal >= 135){ // B: 494Hz
     tone(8, notes[6]);
+  }else if(keyVal >= 100){ // C: 523Hz
+    tone(8, notes[7]);
   }else{
     noTone(8);
   }
