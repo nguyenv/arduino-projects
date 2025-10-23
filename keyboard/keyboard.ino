f1294530f9940c030a4984e5c5a2f029bf4a1270
Vivian Nguyen <yuemannguyen@gmail.com>
2025-10-22 23:24:39 -0700

    Initialize Repo With Keyboard

---
+++ keyboard/keyboard.ino
@@ -0,0 +1,45 @@
+// 7 button keyboard using a uniform resistor ladder (6 x 10kΩ).
+
+// Daisy Bell
+// 4 2 1 0 0 1 2 3 5 4
+// 3 5 2 0 0 1 2 3 5 4
+// 4 5 4 6 5 2 1 4 5 4 3 4 3 2
+// 5 6 6 5 3 4 2 0
+
+// Mary Had a Little Lamb
+// 2 1 0 1 2 2 2 1 1 1 2 4 4
+// 2 1 0 1 2 2 2 2 1 1 2 1 0
+
+// Twinkle Twinkle Little Star
+// 0 0 4 4 5 5 4 3 3 2 2 1 1 0
+// 4 4 3 3 2 2 1 4 4 3 3 2 2 1
+// 0 0 4 4 5 5 4 3 3 2 2 1 1 0
+
+const int notes[] = {262, 294, 330, 349, 392, 440, 494};
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int keyVal = analogRead(A0);
+  Serial.println(keyVal);
+
+  if(keyVal == 1023){      // C: 262Hz
+    tone(8, notes[0]);
+  }else if(keyVal >= 500){ // D: 294Hz
+    tone(8, notes[1]);
+  }else if(keyVal >= 330){ // E: 330Hz
+    tone(8, notes[2]);
+  }else if(keyVal >= 250){ // F: 349Hz
+    tone(8, notes[3]);
+  }else if(keyVal >= 200){ // G: 392Hz
+    tone(8, notes[4]);
+  }else if(keyVal >= 160){ // A: 440Hz
+    tone(8, notes[5]);
+  }else if(keyVal >= 140){ // B: 494Hz
+    tone(8, notes[6]);
+  }else{
+    noTone(8);
+  }
+}
